a5823c9debbed258c04957905695dcc254fac355
Tyom5555 <artyom.shahbazyan05gmail.com>
2023-07-01 04:12:14 -0700

    second commit

--- src/AddUser.jsx
+++ src/AddUser.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from "react";
 
-export default function AddUser() {
+export default function AddUser({onAdd}) {
+  const [form, setForm] = useState({ name: "", surename: "", salary: 70000 });
+  const [eror, setEror] = useState()
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(!form.name || !form.surename || !form.salary){
+      setEror("mutqagreq chisht tvyalnery")
+    }else{
+      onAdd(form);
+      setEror("")
+      setForm({ name: "", surename: "", salary: 70000})
+    }
+  }
   return (
     <div>
+      <h3>User Add</h3>
+      <p style={{color:"red" }}>{eror}</p>
+      <form onSubmit={handleSubmit} action="">
+
+        <div>
+          <label></label>
+          <input
+            type="text"
+            value={form.name}
+            onChange={(e) => setForm({...form, name:e.target.value})}
+          />
+        </div>
+
+        <div>
+          <label></label>
+          <input type="text" 
+          value={form.surename}
+          onChange={(e) => setForm({...form, surename:e.target.value})}
+          />
+        </div>
+        <div>
+          <label></label>
+          <input type="number"
+          value={form.salary}
+          onChange={(e) => setForm({...form, salary:e.target.value})}
+          />
+        </div>
+        <button >Add</button>
         
+      </form>
+
     </div>
-  )
+  );
 }
--- src/App.js
+++ src/App.js
@@ -1,60 +1,60 @@
-import UsersList from './UsersList'
-import AddUser from './AddUser';
-import './App.css';
-import { useState } from 'react';
+import UsersList from "./UsersList";
+import AddUser from "./AddUser";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
   const [users, setUsers] = useState([
     {
       id: Math.random(),
-      name:"Hambardzum",
-      surename:"Lackanyan",
-      salary:100000
-
+      name: "Hambardzum",
+      surename: "Lackanyan",
+      salary: 100000,
     },
     {
       id: Math.random(),
-      name:"Asqanaz",
-      surename:"Garibyan",
-      salary:5600000
-
+      name: "Asqanaz",
+      surename: "Garibyan",
+      salary: 5600000,
     },
     {
       id: Math.random(),
-      name:"Hracin",
-      surename:"Gegjanyan",
-      salary:1100000
-
+      name: "Hracin",
+      surename: "Gegjanyan",
+      salary: 1100000,
     },
-  ])
+  ]);
+  const hndleNewUser = (obj) => {
+    setUsers([...users, { id: Math.random(), ...obj }]);
+  };
 
-const handleSalryUp = (id) =>{
-  let person = users.find((elm) => elm.id === id);
-  if(person){
-    person.salary += 50000
-  }
-  setUsers([...users])
-}
-const handleSalryDown = (id) =>{
-  let person = users.findIndex((elm) => elm.id === id);
-  if(person && person.salary >= 50000){
-    person.salary -= 50000
-  }
-  setUsers([...users])
-}
-const handleSalryDelete = (id) =>{
-  setUsers([...users.filter(elm => elm.id !== id)])
-}
+  const handleSalryUp = (id) => {
+    let person = users.find((elm) => elm.id === id);
+    if (person) {
+      person.salary += 50000;
+    }
+    setUsers([...users]);
+  };
+  const handleSalryDown = (id) => {
+    let person = users.find((elm) => elm.id === id);
+    if (person && person.salary >= 50000) {
+      person.salary -= 50000;
+    }
+    setUsers([...users]);
+  };
+  const handleSalryDelete = (id) => {
+    setUsers([...users.filter((elm) => elm.id !== id)]);
+  };
 
   return (
     <div className="App">
-      <UsersList 
-       users = {users} 
-       onDelete = {handleSalryDelete}
-       onSallaryUp = {handleSalryUp}
-       onSallaryDoWn = {handleSalryDown}
-       />
-      <AddUser/>
+      <UsersList
+        users={users}
+        onDelete={handleSalryDelete}
+        onSallaryUp={handleSalryUp}
+        onSallaryDoWn={handleSalryDown}
+      />
+      <AddUser onAdd={hndleNewUser} />
     </div>
   );
 }
